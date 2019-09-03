716289510ce330e35b637b11d341885bb865f881
Mariana Oliveira <olvr.mariana@gmail.com>
2019-09-03 20:41:04 -0300

    Estilizando um componente - tag style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="foto of fotos">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
         <img :src="foto.url" :alt="foto.titulo">
       </li>
     </ul>
@@ -20,7 +20,7 @@ data(){
   }
 },
   created(){
-
+//consumir uma API através do $http
   this.$http.get('http://localhost:3000/v1/fotos')
   .then(res => res.json())
   .then(fotos => this.fotos = fotos, err => console.log(err));
@@ -29,4 +29,21 @@ data(){
 </script>
 
 <style>
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado{
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item{
+    display: inline-block;
+  }
 </style>
