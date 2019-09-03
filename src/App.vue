8da61580dee1afddf76b77429bb90c9dbfd0c1df
Mariana Oliveira <olvr.mariana@gmail.com>
2019-09-03 19:04:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
