afea1518ba965003d8429319a6beed3bcef0a930
Mariana Oliveira <olvr.mariana@gmail.com>
2019-09-03 22:36:30 -0300

    Isola a marcação e o estilo do painel em um novo componente comportilhavel

--- src/App.vue
+++ src/App.vue
@@ -60,26 +60,4 @@ export default {
   width: 100%;
 }
 
-/* estilo do painel */ 
-
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
 </style>
