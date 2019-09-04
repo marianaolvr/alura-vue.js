6af555201e850a091feb504f1d7b69d8581bfabe
Mariana Oliveira <olvr.mariana@gmail.com>
2019-09-03 22:26:52 -0300

    Cria o painel e estiliza

--- src/App.vue
+++ src/App.vue
@@ -4,46 +4,82 @@
 
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo}}</h2>
+          <div class="painel-conteudo">
+
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+
+          </div>
+        </div>
+
       </li>
     </ul>
   </div>
-
 </template>
 
 <script>
 export default {
-data(){
-  return{
-    titulo: 'Alurapic',
-    fotos: []
+  data() {
+    return {
+      titulo: "Alurapic",
+      fotos: []
+    };
+  },
+  created() {
+    //consumir uma API através do $http
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
   }
-},
-  created(){
-//consumir uma API através do $http
-  this.$http.get('http://localhost:3000/v1/fotos')
-  .then(res => res.json())
-  .then(fotos => this.fotos = fotos, err => console.log(err));
-}
-}
+};
 </script>
 
 <style>
-  .corpo{
-    font-family: Helvetica, sans-serif;
-    width: 96%;
-    margin: 0 auto;
-  }
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
 
-  .centralizado{
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva{
+  width: 100%;
+}
+
+/* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
     text-align: center;
   }
 
-  .lista-fotos{
-    list-style: none;
-  }
-
-  .lista-fotos .lista-fotos-item{
-    display: inline-block;
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
   }
 </style>
