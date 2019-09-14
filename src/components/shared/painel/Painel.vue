bd82b6244b1454b444b5f13178a356be897570fd
Mariana Oliveira <olvr.mariana@gmail.com>
2019-09-14 01:25:18 -0300

    encapsula o estilo do componente: acrescenta scoped na tag style do compontente. Boa pratica: todo componente criado deve ter o scoped na tag style para que o estilo dele seja válido apenas para o próprio elemento

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .painel {
   padding: 0 auto;
@@ -33,5 +33,10 @@ export default {
   margin: 0 0 15px 0;
   padding: 10px;
   text-transform: uppercase;
+}
+
+
+ *{
+  box-shadow: 5px 5px 5px;
 }
 </style>
